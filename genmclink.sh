9636f048cca7a44e2642dc0f6e4b3fcb965fcf5c
hgcvm <none@foo.com>
2023-01-28 10:21:38 +0100

    Update geojson files

--- genmclink.sh
+++ genmclink.sh
@@ -1,6 +1,4 @@
 jsonfile="mcsurfacegr.json"
-linkname="https://mapcomplete.osm.be?userlayout=true#"
 based=`base64 -w0 $jsonfile`
-echo "https://mapcomplete.osm.be?userlayout=true#$based"
 firefox "https://mapcomplete.osm.be?userlayout=true#$based"
 
